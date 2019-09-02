94c16caa10ee313fed0910af2aff8f8ca3671cfd
Vasya N <n1m5spm@yandex.com>
2019-09-02 21:45:59 +0200

    add support for wesnoth-1.15

--- .build/config.sh
+++ .build/config.sh
@@ -3,4 +3,5 @@ not_pushed_ignore=true
 addon_manager_args=("--pbl-key" "icon" "$(cat src/doc/icon.txt)")
 addon_manager_args=("--pbl-key" "title" "$(cat src/doc/addon_name.txt)")
 
-upload_to_wesnoth_versions=("1.14")
+upload_to_wesnoth_versions=(1.14 1.15)
+
